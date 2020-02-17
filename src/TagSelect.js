629e53521a8034582306c3d49fb28e6061fd58c2
Akila Rathnayaka <akila.rathnayaka@saksglobal.com>
2020-02-17 10:28:15 +0530

    customization

--- src/TagSelect.js
+++ src/TagSelect.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  View,
-  ViewPropTypes,
-  StyleSheet
-} from 'react-native'
+import { View, ViewPropTypes, StyleSheet } from 'react-native'
 
 import TagSelectItem from './TagSelectItem'
 
@@ -28,7 +24,7 @@ class TagSelect extends React.Component {
     onItemPress: PropTypes.func,
 
     // Styles
-    containerStyle: ViewPropTypes.style
+    containerStyle: ViewPropTypes.style,
   }
 
   static defaultProps = {
@@ -44,27 +40,38 @@ class TagSelect extends React.Component {
     onMaxError: null,
     onItemPress: null,
 
-    containerStyle: {}
+    containerStyle: {},
   }
 
   state = {
-    value: {}
+    value: {},
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const value = {}
-    this.props.value.forEach((val) => {
+    this.props.value.forEach(val => {
       value[val[[this.props.keyAttr]] || val] = val
     })
 
     this.setState({ value })
+  }
+  componentDidUpdate(prevProps, prevState, prevContext) {
+    if (prevProps !== this.props) {
+      const value = {}
+
+      this.props.value.forEach(val => {
+        value[val[[this.props.keyAttr]] || val] = val
+      })
+
+      this.setState({ value })
+    }
   }
 
   /**
    * @description Return the number of items selected
    * @return {Number}
    */
-  get totalSelected () {
+  get totalSelected() {
     return Object.keys(this.state.value).length
   }
 
@@ -72,7 +79,7 @@ class TagSelect extends React.Component {
    * @description Return the items selected
    * @return {Array}
    */
-  get itemsSelected () {
+  get itemsSelected() {
     const items = []
 
     Object.entries(this.state.value).forEach(([key]) => {
@@ -87,26 +94,27 @@ class TagSelect extends React.Component {
    * @param {Object} item
    * @return {Void}
    */
-  handleSelectItem = (item) => {
+  handleSelectItem = item => {
     const key = item[this.props.keyAttr] || item
 
-    const value = { ...this.state.value }
+    // const value = { ...this.state.value }
+    const value = [] // Edited By AKila Devinda
     const found = this.state.value[key]
 
     // Item is on array, so user is removing the selection
-    if (found) {
-      delete value[key]
-    } else {
-      // User is adding but has reached the max number permitted
-      if (this.props.max && this.totalSelected >= this.props.max) {
-        if (this.props.onMaxError) {
-          return this.props.onMaxError()
-        }
-      }
+    // if (found) {
+    //   delete value[key]
+    // } else {
+    //   // User is adding but has reached the max number permitted
+    //   if (this.props.max && this.totalSelected >= this.props.max) {
+    //     if (this.props.onMaxError) {
+    //       return this.props.onMaxError()
+    //     }
+    //   }
 
-      value[key] = item
-    }
-
+    //   value[key] = item
+    // }
+    value[key] = item
     return this.setState({ value }, () => {
       if (this.props.onItemPress) {
         this.props.onItemPress(item)
@@ -114,22 +122,21 @@ class TagSelect extends React.Component {
     })
   }
 
-  render () {
+  render() {
     return (
-      <View
-        style={[
-          styles.container,
-          this.props.containerStyle
-        ]}
-      >
-        {this.props.data.map((i) => {
+      <View style={[styles.container, this.props.containerStyle]}>
+        {this.props.data.map(i => {
           return (
             <TagSelectItem
               {...this.props}
               label={i[this.props.labelAttr] ? i[this.props.labelAttr] : i}
               key={i[this.props.keyAttr] ? i[this.props.keyAttr] : i}
               onPress={this.handleSelectItem.bind(this, i)}
-              selected={(this.state.value[i[this.props.keyAttr]] || this.state.value[i]) && true}
+              selected={
+                (this.state.value[i[this.props.keyAttr]] ||
+                  this.state.value[i]) &&
+                true
+              }
             />
           )
         })}
@@ -142,8 +149,8 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    alignItems: 'flex-start'
-  }
+    alignItems: 'flex-start',
+  },
 })
 
 export default TagSelect
